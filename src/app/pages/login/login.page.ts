9d3063a6df94a95d3cfa9b50663ccc0ef94b7dd2
Your Name <marcosvanillle@gmail.com>
2020-07-02 02:11:52 -0300

    [ADD]add novo projeto em ionic 4

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlide, IonSlides } from '@ionic/angular';
+import { Keyboard } from '@ionic-native/keyboard/ngx';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+
+export class LoginPage implements OnInit {
+  @ViewChild(IonSlides) slides: IonSlides;
+  public wavesPosition: number = 0;
+  public wavesDifference: number = 80;
+
+  constructor(public Keyboard: Keyboard) { }
+
+  ngOnInit() {
+
+  }
+
+  segmentChanged(event: any) {
+    console.log(event);
+    if (event.detail.value === "login") {
+      this.slides.slidePrev();
+      this.wavesPosition += this.wavesDifference;
+    } else {
+      this.slides.slideNext();
+      this.wavesPosition -= this.wavesDifference;
+    }
+
+  }
+
+}
